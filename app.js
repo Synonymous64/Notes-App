9261829adcbc4e57b40772b247d0e74263c33ea7
Synonymous64 <prajwalurkude007@gmail.com>
2023-03-21 19:22:11 +0530

    routes and authentication via googleOuth

--- app.js
+++ app.js
@@ -2,15 +2,38 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const connectDB = require('./server/config/db');
+const session = require('express-session');
+const passport = require('passport');
+const MongoStore = require('connect-mongo');
 
 const app = express();
 const port = 5000 || process.env.PORT;
 
+app.use(session({
+    secret: 'leyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    }),
+    // cookie: { maxAge: new Date(Date.now() + (3600000)) },
+}));
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static Files
 
+// connectDB
+connectDB();
+
+
+// Static Files
 app.use(express.static('public'));
 
 // Templating Engine
@@ -19,6 +42,7 @@ app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
 // Routes
+app.use('/', require('./server/routes/auth'));
 app.use('/', require('./server/routes/index'));
 app.use('/', require('./server/routes/dashboard'));
 
--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const dashboardController = require('../controllers/dashboardController');
-
+const { isLoggedIn } = require('../middleware/checkAuth');
 // Dashboard routes
-router.get('/dashboard', dashboardController.dashboard);
+router.get('/dashboard', isLoggedIn, dashboardController.dashboard);
 
 module.exports = router;
